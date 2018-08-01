9d4aba4d9ccad9e7bfc9cbe4a49d59d7fe89d8ca
王宏玉 <hongyuwang@pptv.com>
2018-08-01 19:53:27 +0800

    [Feature] 下拉菜单

--- controllers/interface.js
+++ controllers/interface.js
@@ -2,7 +2,7 @@
 const ejs = require('../utils/ejs');
 
 let interface = (ctx, next) => {
-    ctx.response.body = ejs.render('interface', {title: '接口测试'});
+    ctx.response.body = ejs.render('interface', {title: '接口测试', controllers:[{name:'hello'}, {name:'world'}]});
 }
 
 module.exports = {
